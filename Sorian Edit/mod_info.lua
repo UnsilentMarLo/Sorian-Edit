a8990884019fb1b2d7ace9c554c857399abb5531
Marlo Kruse <Plays.xenon@yahoo.de>
2020-04-03 17:25:02 +0200

    Added Master

---
+++ Sorian Edit/mod_info.lua
@@ -0,0 +1,12 @@
+name = "Marlo's Sorian AI edit"
+version = 16
+copyright = "Copyright © 2020, Electronic Farts co"
+description = "This mod aims at repairing the mess that is sorian, technically a new ai since it uses sorian as a base"
+author = "Degulum"
+icon = "/mods/Sorian edit/icon.bmp"
+url = ""
+uid = "e1cfc521-e1ff-1f61-a3d6-235251b468cd"
+
+exclusive = false
+
+ui_only = false
