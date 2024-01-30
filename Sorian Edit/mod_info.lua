efd7bd676cecee1a8f0938fb12120cafee309a76
Marlo <Plays.xenon@yahoo.de>
2024-01-30 23:03:10 +0100

    Update mod_info.lua

--- Sorian Edit/mod_info.lua
+++ Sorian Edit/mod_info.lua
@@ -1,11 +1,11 @@
 name = "Marlo's Sorian AI edit"
-version = 126
+version = 127
 copyright = "Copyright © 2020, Electronic Farts co"
 description = "This mod is a completely revamped version of Sorian, with tons of new system and features. In terms of difficulty its stronger than Uveso but weaker than M27 - works as of 01.17.2024"
 author = "Degulum"
 icon = "/mods/Sorian Edit/icon.bmp"
 url = ""
-uid = "e1cfc762-e10126f-2323-a6221465666645FAF"
+uid = "e1cfc762-e10127f-2323-a6221465666645FAF"
 
 exclusive = false
 
